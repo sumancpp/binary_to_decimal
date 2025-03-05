2cec8985bf68f597d2cc9f6dc6ae209a66397350
Suman Maity <someone.cpp@proton.me>
2025-03-05 21:02:08 +0530

    Update binToDec.cpp

--- binToDec.cpp
+++ binToDec.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
  
-int binToDec(int binNum){
-  int ans=0, pow=1;
-  while(binNum>0){
-    int rem=binNum%10;
-    ans+=rem*pow;
-    pow=pow*2;
-    binNum/=10;
+int binToDec (int binNum) {
+  int ans = 0, pow = 1;
+  while (binNum > 0) {
+    int rem = binNum % 10;
+    ans += rem * pow;
+    pow = pow * 2;
+    binNum /= 10;
   }
   return ans;
 }
